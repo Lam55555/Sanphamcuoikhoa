645bef706b7eb1b7b61479fa1a197336d263b6d9
Lam55555 <nguyenhoanglam121106@gmail.com>
2023-10-15 10:07:55 +0700

    lan3

--- Lam/js/jslogin.js
+++ Lam/js/jslogin.js
@@ -1,22 +1,36 @@
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
-const btnLogin = document.querySelector(".login__signInButton");
+
+const btnLogin = document.getElementById("login__signInButton");
 var bt= "Link dẫn tới bài tập".link("/checkpoint/checkpoint.html")
 // validation form login
 
 btnLogin.addEventListener("click", (e) => {
   e.preventDefault();
-  var user = JSON.parse(localStorage.getItem("accounts"));
-  // if (username.value === ""|| password.value === "") {
-  //   alert("vui lòng không để trống");
-  // } else {
-  //   // const user = JSON.parse(localStorage.getItem('accounts'));
-  //   if (username==accounts.username) {
-  //     alert("Đăng ký thành công")
-  //   } else {
-  //     alert(accounts.username);
-  //   }
-  // }
+  var accounts = [];
+  accounts = JSON.parse(localStorage.getItem("accounts"));
+  const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+if (username === ""|| password === "") {
+  alert("vui lòng không để trống");
+}
+  for(const account of accounts){
+    
+    if(username==account.username&&password==account.password) 
+    {
+   window.location.href="/Lam/html/register.html"
+    }
+        
+    else {
+        console.log("đăng nhập thất bại");
+      }
+      
+    }
+  }
+    // console.log(accounts[i][0]
   
-});
+  //let Giotraicay=[['xoai',2],['dua hau',3]]
+// for(let i=0; i<Giotraicay.length;i++){
+//     console.log("Gio trai cay "+ Giotraicay[i][0] + " co " +Giotraicay[i][1] +" trai")
+// }
+  
+);
 
