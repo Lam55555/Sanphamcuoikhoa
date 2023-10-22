d492252a3ae74be0e19af98dc514f1ee3eafcfa0
Bachs-JSA05 <bachcaoryan1234@gmail.com>
2023-10-22 10:31:28 +0700

    CÒN 2 TUẦN

--- Bach/trang mua đồ/CHINH.js
+++ Bach/trang mua đồ/CHINH.js
@@ -14,3 +14,20 @@ document.addEventListener('DOMContentLoaded', function() {
         loginStatusElement.appendChild(loginLink);
     }
 });
+/*button */
+document.addEventListener('DOMContentLoaded', function() {
+    const products = document.querySelectorAll('.product');
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  
+    products.forEach(product => {
+        const addToCartButton = product.querySelector('.add-to-cart');
+        const productName = product.dataset.name;
+  
+        addToCartButton.addEventListener('click', function() {
+            cart.push(productName);
+            localStorage.setItem('cart', JSON.stringify(cart));
+            alert(`Đã thêm ${productName} vào giỏ hàng!`);
+        });
+    });
+  });
+  /*mua nhiều mặt hàng */
