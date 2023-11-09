732817850aadb33e92cf00891ed5c4962fdad4b5
Lam55555 <nguyenhoanglam121106@gmail.com>
2023-11-10 06:42:41 +0700

    change img

--- Lam/js/jstonghopsanpham.js
+++ Lam/js/jstonghopsanpham.js
@@ -2,124 +2,207 @@ products = {
     data:[
         {
             productName:"Cặp tặng cô giáo",
-            image:"/Lam/img/pngtree-white-gray-wave-abstract-background-soft-design-graphic-banner-background-picture-image_1298688.jpg",
-            price:'45$'
+            image:"/Bach/trang mua đồ/img/BAG12.jpg",
+            price:'45$',
+            link:"/Bach/trang mua đồ/CHINH.html"
         },
         {
             productName:"Phấn Viết Bảng Mic Trắng/ Màu (Hộp 10-100V)",
-            image:'/Lam/img/img.jpg',
-            price:'1$'
+            image:'/Bach/trang mua đồ/img/Bag14.jpg',
+            price:'1$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Cặp tặng cô giáo",
-            image:"/Lam/img/pngtree-white-gray-wave-abstract-background-soft-design-graphic-banner-background-picture-image_1298688.jpg",
-            price:'45$'
+            image:"/Bach/trang mua đồ/img/Bag15.jpg",
+            price:'45$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Phấn Viết Bảng Mic Trắng/ Màu (Hộp 10-100V)",
-            image:'/Lam/img/img.jpg',
-            price:'1$'
+            image:'/Bach/trang mua đồ/img/Bag16.jpg',
+            price:'1$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Cặp tặng cô giáo",
-            image:"/Lam/img/pngtree-white-gray-wave-abstract-background-soft-design-graphic-banner-background-picture-image_1298688.jpg",
-            price:'45$'
+            image:"/Bach/trang mua đồ/img/Bag17.jpg",
+            price:'45$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Phấn Viết Bảng Mic Trắng/ Màu (Hộp 10-100V)",
-            image:'/Lam/img/img.jpg',
-            price:'1$'
+            image:'/Bach/trang mua đồ/img/hoa1.jpg',
+            price:'1$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Cặp tặng cô giáo",
-            image:"/Lam/img/pngtree-white-gray-wave-abstract-background-soft-design-graphic-banner-background-picture-image_1298688.jpg",
-            price:'45'
+            image:"/Bach/trang mua đồ/img/hoa2.jpg",
+            price:'45',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Phấn Viết Bảng Mic Trắng/ Màu (Hộp 10-100V)",
-            image:'/Lam/img/img.jpg',
-            price:'1'
+            image:'/Bach/trang mua đồ/img/hoa3.jpg',
+            price:'1',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Cặp tặng cô giáo",
-            image:"/Lam/img/pngtree-white-gray-wave-abstract-background-soft-design-graphic-banner-background-picture-image_1298688.jpg",
-            price:'45$'
+            image:"/Bach/trang mua đồ/img/BAG1.jpg",
+            price:'45$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Phấn Viết Bảng Mic Trắng/ Màu (Hộp 10-100V)",
-            image:'/Lam/img/img.jpg',
-            price:'1$'
+            image:"/Bach/trang mua đồ/img/BAG1'.jpg",
+            price:'1$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Cặp tặng cô giáo",
-            image:"/Lam/img/pngtree-white-gray-wave-abstract-background-soft-design-graphic-banner-background-picture-image_1298688.jpg",
-            price:'45$'
+            image:"/Bach/trang mua đồ/img/BAG2'.jpg",
+            price:'45$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         {
             productName:"Phấn Viết Bảng Mic Trắng/ Màu (Hộp 10-100V)",
-            image:'/Lam/img/img.jpg',
-            price:'1$'
+            image:"/Bach/trang mua đồ/img/BAG3'.jpg",
+            price:'1$',
+            link:'/Bach/trang mua đồ/CHINH.html'
         },
         
     ]
 }
 
-    for(let i of products.data) {
-        let card =document.createElement("div")
-        card.classList.add('card')
+        for(let i of products.data) {
+            let card =document.createElement("div")
+            card.classList.add('card')
+        
+            let imgContainer = document.createElement("div")
+            imgContainer.classList.add("imgContainer")
+        
+            let image = document.createElement("img")
+            image.setAttribute("src",i.image)
+            // setAttribute lấy thuộc tính của img bên html
+            imgContainer.appendChild(image)
+            // lấy file image nhét cô imgContainer
+            card.appendChild(imgContainer)
+            image.classList.add('imgs')
+        
+            let container =document.createElement("div")
+            container.classList.add("container")
+        
+            let name=document.createElement("a")
+            name.classList.add("product-name")
+            name.innerText = i.productName.toUpperCase()
+            name.setAttribute('href',i.link)
+            container.appendChild(name)
+        
+            let price=document.createElement("h6")
+            price.innerText = "$" + i.price
+            container.appendChild(price)
+        
+            card.appendChild(container)
     
-        let imgContainer = document.createElement("div")
-        imgContainer.classList.add("imgContainer")
-    
-        let image = document.createElement("img")
-        image.setAttribute("src",i.image)
-        // setAttribute lấy thuộc tính của img bên html
-        imgContainer.appendChild(image)
-        // lấy file image nhét cô imgContainer
-        card.appendChild(imgContainer)
-        image.classList.add('imgs')
-    
-        let container =document.createElement("div")
-        container.classList.add("container")
-    
-        let name=document.createElement("h5")
-        name.classList.add("product-name")
-        name.innerText = i.productName.toUpperCase()
-        container.appendChild(name)
-    
-        let price=document.createElement("h6")
-        price.innerText = "$" + i.price
-        container.appendChild(price)
-    
-        card.appendChild(container)
+            
+            
+
+            let tong = document.createElement('div')
+            tong.appendChild(card) //card thuoc tong
+            tong.appendChild(container) //container thuoc tong
+            tong.classList.add('tong')
+            document.getElementById("product").appendChild(tong)
+            
+
+            //thêm thẻ <a> vào div tong
+            let lienket = document.createElement('a')
+            lienket.setAttribute('href',i.link)
+            lienket.classList.add('linksp')
+            tong.appendChild(lienket)
+
+            
 
 
-        let tong = document.createElement('div')
-        tong.appendChild(card) //card thuoc tong
-        tong.appendChild(container) //container thuoc tong
-        tong.classList.add('tong')
-        document.getElementById("product").appendChild(tong)
+
+
+
+
+
+
+            
+        }
+
+
+ 
+
     
-    }
-
-
-    document.getElementById("search").addEventListener("click",() =>{
-        let searchInput = document.getElementById("search-input").value.toUpperCase()
-        let tong= document.querySelectorAll(".tong")
-        let productName = document.querySelectorAll(".product-name")
-        let prices = document.querySelectorAll("h6")
+// function trang2(){
+//     for(let i of products.data2) {
+//         let card =document.createElement("div")
+//         card.classList.add('card')
     
-       productName.forEach((items, index)=> {
-        if (items.innerText.includes(searchInput)){
-            tong[index].classList.remove("hide")
-            } else{
-                tong[index].classList.add("hide")
-            }
-       })
-    })
+//         let imgContainer = document.createElement("div")
+//         imgContainer.classList.add("imgContainer")
+    
+//         let image = document.createElement("img")
+//         image.setAttribute("src",i.image)
+//         // setAttribute lấy thuộc tính của img bên html
+//         imgContainer.appendChild(image)
+//         // lấy file image nhét cô imgContainer
+//         card.appendChild(imgContainer)
+//         image.classList.add('imgs')
+    
+//         let container =document.createElement("div")
+//         container.classList.add("container")
+    
+//         let name=document.createElement("h5")
+//         name.classList.add("product-name")
+//         name.innerText = i.productName.toUpperCase()
+//         container.appendChild(name)
+    
+//         let price=document.createElement("h6")
+//         price.innerText = "$" + i.price
+//         container.appendChild(price)
+    
+//         card.appendChild(container)
 
 
-    let slideIndex = 0;
+//         let tong = document.createElement('div')
+//         tong.appendChild(card) //card thuoc tong
+//         tong.appendChild(container) //container thuoc tong
+//         tong.classList.add('tong')
+//         document.getElementById("product").appendChild(tong)
+    
+//     }
+//     trang1.classList.add('hide')
+// }
+
+document.getElementById("search").addEventListener("click",() =>{
+    let searchInput = document.getElementById("search-input").value.toUpperCase()
+    let tong= document.querySelectorAll(".tong")
+    let productName = document.querySelectorAll(".product-name")
+    let prices = document.querySelectorAll("h6")
+
+   productName.forEach((items, index)=> {
+    if (items.innerText.includes(searchInput)){
+        tong[index].classList.remove("hide")
+        } else{
+            tong[index].classList.add("hide")
+        }
+   })
+})
+
+// var ckavt=document.getElementById('ckavt');
+// var checkavt = localStorage.getItem('avatar')
+// var images = document.createElement('img');
+// images.classList.add('img-avatar')
+// images.setAttribute('src', checkavt)
+// ckavt.appendChild(images);
+
+
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -138,5 +221,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
